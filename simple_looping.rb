5fcd54d8dc5ac7fa0f9e3ec209c99231258ae4b4
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-23 03:01:38 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -4,8 +4,8 @@ def loop_iterator(number_of_times)
   counter = 0
   loop do
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  break if counter==number_of_times
   counter+=1
+  break if counter==number_of_times
   end
 end
 
